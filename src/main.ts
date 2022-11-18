352cd3031df0da30dfa404c76395b9a51ecaa898
Julia <earnkay2@gmail.com>
2022-11-18 15:58:06 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import store from './store'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.js'
 import axios from 'axios'
-// import VueAxios from 'vue-axios'
 
 const app = createApp(App);
 
-// app.use(VueAxios, axios)
 app.provide('axios', app.config.globalProperties.axios)
 
 createApp(App).use(store).use(router).mount('#app')
